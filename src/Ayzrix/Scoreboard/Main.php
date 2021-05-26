58ee48c0a982d20a4b91c0fbb513496d719ca1a9
Ayzrix <32325042+AyzrixYTB@users.noreply.github.com>
2021-05-26 22:33:02 +0200

    Remake extensions enabling system

--- src/Ayzrix/Scoreboard/Main.php
+++ src/Ayzrix/Scoreboard/Main.php
@@ -23,20 +23,8 @@ class Main extends PluginBase {
     /** @var Main $instance */
     private static $instance;
 
-    public static $piggyfaction = false;
-    public static $factionspro = false;
-    public static $simplefaction = false;
-    public static $economyapi = false;
-    public static $pureperms = false;
-    public static $skyblock = false;
-    public static $seedevice = false;
-    public static $bounty = false;
-    public static $prison = false;
-    public static $combatlogger = false;
-    public static $onlinetime = false;
-    public static $fightlogger = false;
-    public static $myplot = false;
-    public static $coinssystem = false;
+    /** @var array $options */
+    public static $options = [];
 
     public function onEnable(){
         $this->saveDefaultConfig();
@@ -47,131 +35,15 @@ class Main extends PluginBase {
     }
 
     private function checkDependencies(): void {
-
-        if (Utils::getIntoConfig("options")["PiggyFaction"] === true) {
-            $piggyfaction = $this->getServer()->getPluginManager()->getPlugin("PiggyFactions");
-            if(is_null($piggyfaction)) {
-                $this->getLogger()->notice("Please download a valid version of PiggyFaction");
-                $this->getServer()->getPluginManager()->disablePlugin($this);
-                return;
-            } else self::$piggyfaction = true;
-        }
-
-        if (Utils::getIntoConfig("options")["FactionsPro"] === true) {
-            $factionspro = $this->getServer()->getPluginManager()->getPlugin("FactionsPro");
-            if(is_null($factionspro)) {
-                $this->getLogger()->notice("Please download a valid version of FactionsPro");
-                $this->getServer()->getPluginManager()->disablePlugin($this);
-                return;
-            } else self::$factionspro = true;
-        }
-
-        if (Utils::getIntoConfig("options")["SimpleFaction"] === true) {
-            $simplefaction = $this->getServer()->getPluginManager()->getPlugin("SimpleFaction");
-            if(is_null($simplefaction)) {
-                $this->getLogger()->notice("Please download a valid version of SimpleFaction");
-                $this->getServer()->getPluginManager()->disablePlugin($this);
-                return;
-            } else self::$simplefaction = true;
-        }
-
-        if (Utils::getIntoConfig("options")["EconomyAPI"] === true) {
-            $economyapi = $this->getServer()->getPluginManager()->getPlugin("EconomyAPI");
-            if(is_null($economyapi)) {
-                $this->getLogger()->notice("Please download a valid version of EconomyAPI");
-                $this->getServer()->getPluginManager()->disablePlugin($this);
-                return;
-            } else self::$economyapi = true;
-        }
-
-        if (Utils::getIntoConfig("options")["PurePerms"] === true) {
-            $pureperms = $this->getServer()->getPluginManager()->getPlugin("PurePerms");
-            if(is_null($pureperms)) {
-                $this->getLogger()->notice("Please download a valid version of PurePerms");
-                $this->getServer()->getPluginManager()->disablePlugin($this);
-                return;
-            } else self::$pureperms = true;
-        }
-
-        if (Utils::getIntoConfig("options")["SkyBlock"] === true) {
-            $skyblock = $this->getServer()->getPluginManager()->getPlugin("SkyBlock");
-            if(is_null($skyblock)) {
-                $this->getLogger()->notice("Please download a valid version of SkyBlock");
-                $this->getServer()->getPluginManager()->disablePlugin($this);
-                return;
-            } else self::$skyblock = true;
-        }
-
-        if (Utils::getIntoConfig("options")["SeeDevice"] === true) {
-            $seedevice = $this->getServer()->getPluginManager()->getPlugin("SeeDevice");
-            if(is_null($seedevice)) {
-                $this->getLogger()->notice("Please download a valid version of SeeDevice");
-                $this->getServer()->getPluginManager()->disablePlugin($this);
-                return;
-            } else self::$seedevice = true;
-        }
-
-        if (Utils::getIntoConfig("options")["Bounty"] === true) {
-            $bounty = $this->getServer()->getPluginManager()->getPlugin("Bounty");
-            if(is_null($bounty)) {
-                $this->getLogger()->notice("Please download a valid version of Bounty");
-                $this->getServer()->getPluginManager()->disablePlugin($this);
-                return;
-            } else self::$bounty = true;
-        }
-
-        if (Utils::getIntoConfig("options")["Prisons"] === true) {
-            $prison = $this->getServer()->getPluginManager()->getPlugin("Prisons");
-            if(is_null($prison)) {
-                $this->getLogger()->notice("Please download a valid version of Prisons");
-                $this->getServer()->getPluginManager()->disablePlugin($this);
-                return;
-            } else self::$prison = true;
-        }
-
-        if (Utils::getIntoConfig("options")["OnlineTime"] === true) {
-            $onlinetime = $this->getServer()->getPluginManager()->getPlugin("OnlineTime");
-            if(is_null($onlinetime)) {
-                $this->getLogger()->notice("Please download a valid version of OnlineTime");
-                $this->getServer()->getPluginManager()->disablePlugin($this);
-                return;
-            } else self::$onlinetime = true;
-        }
-
-        if (Utils::getIntoConfig("options")["CombatLogger"] === true) {
-            $combatlogger = $this->getServer()->getPluginManager()->getPlugin("CombatLogger");
-            if(is_null($combatlogger)) {
-                $this->getLogger()->notice("Please download a valid version of CombatLogger");
-                $this->getServer()->getPluginManager()->disablePlugin($this);
-                return;
-            } else self::$combatlogger = true;
-        }
-
-        if (Utils::getIntoConfig("options")["FightLogger"] === true) {
-            $fightlogger = $this->getServer()->getPluginManager()->getPlugin("FightLogger");
-            if(is_null($fightlogger)) {
-                $this->getLogger()->notice("Please download a valid version of FightLogger");
-                $this->getServer()->getPluginManager()->disablePlugin($this);
-                return;
-            } else self::$fightlogger = true;
-        }
-
-        if (Utils::getIntoConfig("options")["MyPlot"] === true) {
-            $myplot = $this->getServer()->getPluginManager()->getPlugin("MyPlot");
-            if(is_null($myplot)) {
-                $this->getLogger()->notice("Please download a valid version of MyPlot");
-                $this->getServer()->getPluginManager()->disablePlugin($this);
-                return;
-            } else self::$myplot = true;
-        }
-
-        if (Utils::getIntoConfig("options")["CoinsSystem"] === true) {
-            $coinssystem = $this->getServer()->getPluginManager()->getPlugin("CoinsSystem");
-            if(is_null($coinssystem)) {
-                $this->getLogger()->notice("Please download a valid version of CoinsSystem");
-                $this->getServer()->getPluginManager()->disablePlugin($this);
-                return;
-            } else self::$coinssystem = true;
+        foreach (Utils::getIntoConfig("options") as $pluginName => $bool) {
+            if ($bool === true) {
+                $plugin = $this->getServer()->getPluginManager()->getPlugin($pluginName);
+                if (is_null($plugin)) {
+                    $this->getLogger()->notice("Please download a valid version of {$pluginName}");
+                    $this->getServer()->getPluginManager()->disablePlugin($this);
+                    return;
+                } else self::$options[$pluginName] = true;
+            } else self::$options[$pluginName] = false;
         }
     }
 
--- src/Ayzrix/Scoreboard/Utils/Utils.php
+++ src/Ayzrix/Scoreboard/Utils/Utils.php
@@ -19,6 +19,7 @@ use Ayzrix\Scoreboard\Extensions\CombatLogger;
 use Ayzrix\Scoreboard\Extensions\EconomyAPI;
 use Ayzrix\Scoreboard\Extensions\FactionsPro;
 use Ayzrix\Scoreboard\Extensions\FightLogger;
+use Ayzrix\Scoreboard\Extensions\KDR;
 use Ayzrix\Scoreboard\Extensions\MyPlot;
 use Ayzrix\Scoreboard\Extensions\OnlineTime;
 use Ayzrix\Scoreboard\Extensions\PiggyFaction;
@@ -27,6 +28,7 @@ use Ayzrix\Scoreboard\Extensions\PurePerms;
 use Ayzrix\Scoreboard\Extensions\SeeDevice;
 use Ayzrix\Scoreboard\Extensions\SimpleFaction;
 use Ayzrix\Scoreboard\Extensions\Skyblock;
+use Ayzrix\Scoreboard\Extensions\VoteParty;
 use Ayzrix\Scoreboard\Main;
 use pocketmine\Player;
 use pocketmine\Server;
@@ -49,20 +51,22 @@ class Utils {
      */
     public static function formateString(Player $player, string $string): string {
         $string = str_replace(["{ping}", "{tps}", "{name}", "{online}", "{max_online}", "{level}", "{x}", "{y}", "{z}", "{ip}", "{port}", "{uid}", "{xuid}", "{health}", "{max_health}", "{food}", "{max_food}", "{gamemode}", "{scale}"], [$player->getPing(), Server::getInstance()->getTicksPerSecond(), $player->getName(), count(Server::getInstance()->getOnlinePlayers()), Server::getInstance()->getMaxPlayers(), $player->getLevel()->getFolderName(), round($player->getX()), round($player->getY()), round($player->getZ()), $player->getAddress(), $player->getPort(), $player->getUniqueId(), $player->getXuid(), $player->getHealth(), $player->getMaxHealth(), $player->getFood(), $player->getMaxFood(), $player->getGamemode(), $player->getScale()], $string);
-        if (Main::$piggyfaction === true) $string = str_replace(["{faction_name}", "{faction_rank}", "{faction_power}"], [PiggyFaction::getPlayerFaction($player), PiggyFaction::getPlayerRank($player), PiggyFaction::getFactionPower($player)], $string);
-        if (Main::$factionspro === true) $string = str_replace(["{faction_name}", "{faction_power}"], [FactionsPro::getPlayerFaction($player), FactionsPro::getFactionPower($player)], $string);
-        if (Main::$simplefaction === true) $string = str_replace(["{faction_name}", "{faction_rank}", "{faction_power}", "{faction_money}"], [SimpleFaction::getPlayerFaction($player), SimpleFaction::getPlayerRank($player), SimpleFaction::getFactionPower($player), SimpleFaction::getFactionMoney($player)], $string);
-        if (Main::$economyapi === true) $string = str_replace(["{money}"], [EconomyAPI::getMoney($player)], $string);
-        if (Main::$pureperms === true) $string = str_replace(["{rank}", "{prefix}", "{suffix}"], [PurePerms::getPlayerRank($player), PurePerms::getPlayerPrefix($player), PurePerms::getPlayerSuffix($player)], $string);
-        if (Main::$skyblock === true) $string = str_replace(["{island_blocks}", "{island_members}", "{island_rank}", "{island_size}"], [Skyblock::getIslandBlocks($player), Skyblock::getIslandMembers($player), Skyblock::getIslandRank($player), Skyblock::getIslandSize($player)], $string);
-        if (Main::$seedevice === true) $string = str_replace(["{device}"], [SeeDevice::getPlayerOs($player)], $string);
-        if (Main::$bounty === true) $string = str_replace(["{bounty}"], [Bounty::getPlayerBounty($player)], $string);
-        if (Main::$prison === true) $string = str_replace(["{prisons_rank}", "{prisons_prestige}"], [Prisons::getPlayerRank($player), Prisons::getPlayerPrestige($player)], $string);
-        if (Main::$onlinetime === true) $string = str_replace(["{onlinetime_session}", "{onlinetime_total}"], [OnlineTime::getSessionTime($player), OnlineTime::getTotalTime($player)], $string);
-        if (Main::$combatlogger === true) $string = str_replace(["{combatlogger_time}"], [CombatLogger::getTaggedTime($player)], $string);
-        if (Main::$fightlogger === true) $string = str_replace(["{fightlogger_time}"], [FightLogger::getTaggedTime($player)], $string);
-        if (Main::$myplot === true) $string = str_replace(["{myplot_owner}", "{myplot_id}"], [MyPlot::getPlotOwner($player), MyPlot::getPlotID($player)], $string);
-        if (Main::$coinssystem === true) $string = str_replace(["{coins}"], [CoinsSystem::getPlayerCoins($player)], $string);
+        if (Main::$options["PiggyFactions"] === true) $string = str_replace(["{faction_name}", "{faction_rank}", "{faction_power}"], [PiggyFaction::getPlayerFaction($player), PiggyFaction::getPlayerRank($player), PiggyFaction::getFactionPower($player)], $string);
+        if (Main::$options["FactionsPro"] === true) $string = str_replace(["{faction_name}", "{faction_power}"], [FactionsPro::getPlayerFaction($player), FactionsPro::getFactionPower($player)], $string);
+        if (Main::$options["SimpleFaction"] === true) $string = str_replace(["{faction_name}", "{faction_rank}", "{faction_power}", "{faction_money}"], [SimpleFaction::getPlayerFaction($player), SimpleFaction::getPlayerRank($player), SimpleFaction::getFactionPower($player), SimpleFaction::getFactionMoney($player)], $string);
+        if (Main::$options["EconomyAPI"] === true) $string = str_replace(["{money}"], [EconomyAPI::getMoney($player)], $string);
+        if (Main::$options["PurePerms"] === true) $string = str_replace(["{rank}", "{prefix}", "{suffix}"], [PurePerms::getPlayerRank($player), PurePerms::getPlayerPrefix($player), PurePerms::getPlayerSuffix($player)], $string);
+        if (Main::$options["SkyBlock"] === true) $string = str_replace(["{island_blocks}", "{island_members}", "{island_rank}", "{island_size}"], [Skyblock::getIslandBlocks($player), Skyblock::getIslandMembers($player), Skyblock::getIslandRank($player), Skyblock::getIslandSize($player)], $string);
+        if (Main::$options["SeeDevice"] === true) $string = str_replace(["{device}"], [SeeDevice::getPlayerOs($player)], $string);
+        if (Main::$options["Bounty"] === true) $string = str_replace(["{bounty}"], [Bounty::getPlayerBounty($player)], $string);
+        if (Main::$options["Prisons"] === true) $string = str_replace(["{prisons_rank}", "{prisons_prestige}"], [Prisons::getPlayerRank($player), Prisons::getPlayerPrestige($player)], $string);
+        if (Main::$options["OnlineTime"] === true) $string = str_replace(["{onlinetime_session}", "{onlinetime_total}"], [OnlineTime::getSessionTime($player), OnlineTime::getTotalTime($player)], $string);
+        if (Main::$options["CombatLogger"] === true) $string = str_replace(["{combatlogger_time}"], [CombatLogger::getTaggedTime($player)], $string);
+        if (Main::$options["FightLogger"] === true) $string = str_replace(["{fightlogger_time}"], [FightLogger::getTaggedTime($player)], $string);
+        if (Main::$options["MyPlot"] === true) $string = str_replace(["{myplot_owner}", "{myplot_id}"], [MyPlot::getPlotOwner($player), MyPlot::getPlotID($player)], $string);
+        if (Main::$options["CoinsSystem"] === true) $string = str_replace(["{coins}"], [CoinsSystem::getPlayerCoins($player)], $string);
+        if (Main::$options["KDR"] === true) $string = str_replace(["{kills}", "{deaths}", "{kdr}"], [KDR::getPlayerKills($player), KDR::getPlayerDeaths($player), KDR::getPlayerKDR($player)], $string);
+        if (Main::$options["VoteParty"] === true) $string = str_replace(["{votes}", "{maxvotes}"], [VoteParty::getVotes(), VoteParty::getMaxVotes()], $string);
         return $string;
     }
 }
